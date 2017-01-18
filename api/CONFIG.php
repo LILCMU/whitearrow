b91522bd68c2b400e69d13ac28be7fea08d18f07
Tananut Panyogosa <tongtananut@gmail.com>
2017-01-18 17:44:58 +0700

    update path location

--- api/CONFIG.php
+++ api/CONFIG.php
@@ -1,8 +1,8 @@
 <?php
 $_CONFIG = array();
-$_CONFIG['COMMON'] ['DEBUG']    = false;
-$_CONFIG['COMMON'] ['BASE_URL'] = $_SERVER['HTTP_HOST'] . "/nsc2017/";
-$_CONFIG['URL']    ['M_OFFSET'] = 1;
+$_CONFIG['COMMON'] ['DEBUG']    = true;
+$_CONFIG['COMMON'] ['BASE_URL'] = $_SERVER['HTTP_HOST'] . "/nsc2017/api/";
+$_CONFIG['URL']    ['M_OFFSET'] = 2;
 
 $_CONFIG['DB']['HOST']     = "localhost";
 $_CONFIG['DB']['USERNAME'] = "root";

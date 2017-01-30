fd4a472b0da440398c5648b56a12b6b13ed0877c
Tananut Panyogosa <tongtananut@gmail.com>
2017-01-30 13:00:42 +0700

    Set default database

--- api/CONFIG.php
+++ api/CONFIG.php
@@ -1,12 +1,12 @@
 <?php
 $_CONFIG = array();
-$_CONFIG['COMMON'] ['DEBUG']    = false;
+$_CONFIG['COMMON'] ['DEBUG']    = true;
 $_CONFIG['COMMON'] ['BASE_URL'] = $_SERVER['HTTP_HOST'] . "/nsc2017/api/";
 $_CONFIG['URL']    ['M_OFFSET'] = 2;
 
 $_CONFIG['DB']['HOST']     = "localhost";
 $_CONFIG['DB']['USERNAME'] = "root";
-$_CONFIG['DB']['PASSWORD'] = "";
+$_CONFIG['DB']['PASSWORD'] = "ilovecpe";
 $_CONFIG['DB']['NAME']     = "nsc2017";
 
 ?>

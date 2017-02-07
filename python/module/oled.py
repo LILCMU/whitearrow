fe976786d7b799de40a4747337b02101725929e9
Peeranut Pongpakatien <peeranut.31@hotmail.com>
2017-02-07 20:16:04 +0700

    oled V1.0

--- python/module/oled.py
+++ python/module/oled.py
@@ -1,5 +1,5 @@
 import ssd1306, machine
-i2c = machine.I2C(scl=machine.Pin(5),sda=machine.Pin(4))
+i2c = machine.I2C(scl=machine.Pin(13),sda=machine.Pin(5))
 oled = ssd1306.SSD1306_I2C(128,64,i2c)
 print('Init display Ready !')
 
@@ -12,11 +12,12 @@ def header(string):
     oled.show()
 
 def body(string):
-    oled.text(string,0,20)
+    oled.text(string,0,16)
     oled.show()
 
 def text(string,w,h):
     oled.text(string,w,h)
+    oled.show()
 
 def show():
     oled.show()

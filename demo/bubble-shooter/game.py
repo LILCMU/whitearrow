c515b48ac05def71c9a532ea70a20d63c3d5a504
Tananut Panyogosa <tongtananut@gmail.com>
2017-02-14 02:23:25 +0700

    Add demo3

---
+++ demo/bubble-shooter/game.py
@@ -0,0 +1,18 @@
+import hmc5883l
+import time,math
+import ubinascii
+import ujson
+from umqtt.simple import MQTTClient
+from machine import ADC,unique_id
+sensor = hmc5883l.HMC5883L()
+
+def main():
+	CLIENT_ID = ubinascii.hexlify(unique_id())
+	mqtt = MQTTClient(CLIENT_ID,"broker.mqttdashboard.com")
+	mqtt.connect()
+	while True:
+		value1 = sensor.axes()[1]
+		value2 = ADC(0).read()
+		json = {"value1": value1, "value2": value2}
+		mqtt.publish('NSC2017',ujson.dumps(json),retain=True)
+		time.sleep(0.2)
